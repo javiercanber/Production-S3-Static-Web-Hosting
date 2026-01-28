9bc1c555fcc398424c2167f82b448a61a6c89811
Javier <javier.candela.berrocal@gmail.com>
2026-01-28 17:34:45 +0100

    Fix cloudns output file

--- modules/cloudns/outputs.tf
+++ modules/cloudns/outputs.tf
@@ -1,3 +1,3 @@
 output "validation_record_fqdns" {
-  value = [for record in cloudns_dns_record.cert_validation : "${record.host}.${var.zone_name}"]
+  value = [for record in cloudns_dns_record.cert_validation : "${record.name}.${var.zone_name}"]
 }
